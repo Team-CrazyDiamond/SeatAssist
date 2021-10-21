b47c17d29752a339b05aff54d3066553324d8284
Tomoch0104 <to0104mo2517@icloud.com>
2021-10-21 11:32:47 +0900

    テストコード記述しているバージョン

--- main/app/src/main/java/com/example/seatassist/ui/start/StartScreen.kt
+++ main/app/src/main/java/com/example/seatassist/ui/start/StartScreen.kt
@@ -1,13 +1,18 @@
 package com.example.seatassist.ui.start
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,6 +21,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.seatassist.R
+import com.example.seatassist.ui.components.SAAlertDialog
+import kotlin.reflect.KProperty0
 
 /**
  * スタート画面
@@ -23,10 +30,9 @@ import com.example.seatassist.R
 @ExperimentalMaterialApi
 @Composable
 fun StartScreen(
-    onUsageClick:() -> Unit,
-    onStartClick:() -> Unit
+    onUsageClick: () -> Unit,
+    onStartClick: () -> Unit
 ) {
-
     Scaffold(
         backgroundColor = MaterialTheme.colors.primary,
         contentColor = contentColorFor(MaterialTheme.colors.primary)
@@ -38,6 +44,9 @@ fun StartScreen(
             verticalArrangement = Arrangement.Center
         )
         {
+            var openDialog = remember {
+                mutableStateOf(false)
+            }
             Text(
                 text = "Seat Assist",
                 fontSize = 60.sp,
@@ -58,9 +67,35 @@ fun StartScreen(
             }
             StartButton("Start", onStartClick)    // スタートボタン
             HowToUse("How to use this App", onUsageClick) // 使い方ボタン
+            DialogTest(openDialog)
         }
     }
 
+}
+
+@Composable
+fun DialogTest(openDialog: MutableState<Boolean>) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = { openDialog.value = true })
+    ) {
+        Text(
+            text = "Show toast",
+            color = MaterialTheme.colors.onPrimary,
+            style = MaterialTheme.typography.h4,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+    if (openDialog.value) {
+        SAAlertDialog(
+            title = "title alert",
+            text = "this is alert text",
+            primaryColor = MaterialTheme.colors.primary,
+            onPrimaryColor = MaterialTheme.colors.onPrimary,
+            openDialog = openDialog
+        )
+    }
 }
 
 /**
