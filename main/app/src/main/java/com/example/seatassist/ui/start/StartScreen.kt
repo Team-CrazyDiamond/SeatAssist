65429e535feba73d439c5a3b8aa746191e8fd523
Tomoch0104 <to0104mo2517@icloud.com>
2021-10-24 23:44:26 +0900

    エラー処理

--- main/app/src/main/java/com/example/seatassist/ui/start/StartScreen.kt
+++ main/app/src/main/java/com/example/seatassist/ui/start/StartScreen.kt
@@ -1,7 +1,6 @@
 package com.example.seatassist.ui.start
 
-
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
